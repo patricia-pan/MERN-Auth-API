6e404df877cf825769a430327565160d534a2df3
Your Name <you@example.com>
2021-02-17 14:56:18 -0800

    Added server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
-// mongoDB url: 
-// mongodb+srv://sei:<password>@sei-mern-auth.nd6lm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
+// Allows us to use res.json (a method provided by express, and adding the below line allows us to access it) instead of res.send. 
+app.use(express.json())
+
+// Allows access from all origins. Means that you can access API info from sources other than this folder.
+app.use(cors())
+
+// bodyparser middleware
+app.use(express.urlencoded({extended: false}))
+
+
+// We use 'process.env.PORT' for if heroku wants to use its own port.
+app.listen(process.env.PORT || 8000, () => {
+    console.log("Listening on port 8000") // Putting our server on a different port since our client, React, defaults to port 3000.
+})
